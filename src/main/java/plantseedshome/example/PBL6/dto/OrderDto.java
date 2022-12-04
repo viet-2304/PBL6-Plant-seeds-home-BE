ebeff167212f70d7cc09b1b9e2e02aa4068485bf
viet pham <vietphama7@gmail.com>
2022-12-04 17:03:20 +0700

    remove product in cartDto

---
+++ src/main/java/plantseedshome/example/PBL6/dto/OrderDto.java
@@ -0,0 +1,13 @@
+package plantseedshome.example.PBL6.dto;
+
+import java.util.Date;
+
+public class OrderDto {
+    private String orderId;
+    private int number;
+    private String total;
+    private Date updateDate;
+    private Date createDate;
+    private String paymentMethod;
+    private String orderStatus;
+}
