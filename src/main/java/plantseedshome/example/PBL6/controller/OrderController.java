a0680f6f02a94641ffc5ae0eb840c44bfc0a0c2b
viet-2304 <vietphama7@gmail.com>
2022-12-09 18:36:07 +0700

    Create API get All payment method and get orderDto from create new order

--- src/main/java/plantseedshome/example/PBL6/controller/OrderController.java
+++ src/main/java/plantseedshome/example/PBL6/controller/OrderController.java
@@ -1,15 +1,22 @@
 package plantseedshome.example.PBL6.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import plantseedshome.example.PBL6.dto.OrderDto;
 
 @Controller
 @RestController
 @RequestMapping("/api/v1/order")
 @CrossOrigin
 public class OrderController {
+
+    @PostMapping("/CreateOrder")
+    public ResponseEntity<String> CreateNewOrder(@RequestBody OrderDto orderDto) {
+        System.out.println(orderDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 
 }
